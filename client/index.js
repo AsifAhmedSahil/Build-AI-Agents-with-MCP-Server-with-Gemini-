e42b83b2ddadaa603d1185dd924de200b6d5cd72
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-04-10 23:53:09 +0600

    AI model gemini api intrgrated and also chat with ai - generate response:

--- client/index.js
+++ client/index.js
@@ -1,5 +1,14 @@
 const readline = require('readline/promises')
 
+require('dotenv').config();
+const { GoogleGenAI } =require( "@google/genai"); 
+const { text } = require('stream/consumers');
+const { type } = require('os');
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+
+
 const chatHistory = []
 const rl = readline.createInterface({
     input:process.stdin,
@@ -9,5 +18,24 @@ const rl = readline.createInterface({
 async function chatLoop() {
     const question = await rl.question('You:')
 
+    chatHistory.push({
+        role:"user",
+        parts: [
+            {
+                text: question,
+                type:"text"
+            }
+        ]
+    })
+
+    const response = await ai.models.generateContent({
+        model:"gemini-2.0-flash",
+        contents:chatHistory
+    })
+
+    console.log(response.candidates[0].content.parts[0].text)
+
     
 }
+
+chatLoop()
