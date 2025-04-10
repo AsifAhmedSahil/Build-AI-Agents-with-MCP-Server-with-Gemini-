08d79a35285be1022c505745e7a218756a650422
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-04-11 00:37:18 +0600

    setup server and mcp:

--- client/index.js
+++ client/index.js
@@ -1,41 +1,52 @@
-const readline = require('readline/promises')
+import { config } from "dotenv";
+import readline from "readline/promises";
 
-require('dotenv').config();
-const { GoogleGenAI } =require( "@google/genai"); 
-const { text } = require('stream/consumers');
-const { type } = require('os');
+import { GoogleGenAI } from "@google/genai";
+const { text } = require("stream/consumers");
+const { type } = require("os");
+config();
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-
-
-const chatHistory = []
+const chatHistory = [];
 const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function chatLoop() {
-    const question = await rl.question('You:')
+  const question = await rl.question("You:");
 
-    chatHistory.push({
-        role:"user",
-        parts: [
-            {
-                text: question,
-                type:"text"
-            }
-        ]
-    })
+  chatHistory.push({
+    role: "user",
+    parts: [
+      {
+        text: question,
+        type: "text",
+      },
+    ],
+  });
 
-    const response = await ai.models.generateContent({
-        model:"gemini-2.0-flash",
-        contents:chatHistory
-    })
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: chatHistory,
+  });
 
-    console.log(response.candidates[0].content.parts[0].text)
+  const responseText = response.candidates[0].content.parts[0].text;
 
-    
+  chatHistory.push({
+    role: "model",
+    parts: [
+      {
+        text: responseText,
+        type: "text",
+      },
+    ],
+  });
+
+  console.log(`AI ${responseText}`);
+
+  chatLoop();
 }
 
-chatLoop()
+chatLoop();
